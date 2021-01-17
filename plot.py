489f7d3d48ab631ad48117e53a7364983aed065a
Xintong <xintongyang1994@foxmail.com>
2021-01-17 23:31:24 +0000

    plt agg

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import json
 import numpy as np
 import matplotlib as mpl
-# mpl.use('Agg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from copy import deepcopy as dcp
 from mpl_toolkits.axes_grid1 import make_axes_locatable, axes_size
