5120576184a301392ba86693eca95f25c6266af1
Xintong <xintongyang1994@foxmail.com>
2023-08-18 11:24:24 +0100

    clean up unused scripts

--- drl_implementation/__init__.py
+++ drl_implementation/__init__.py
@@ -4,8 +4,6 @@ from .agent.continuous_action.sac import SAC
 from .agent.continuous_action.sac_goal_conditioned import GoalConditionedSAC
 from .agent.continuous_action.td3 import TD3
 from .agent.continuous_action.distributional_ddpg import DistributionalDDPG
-from .agent.continuous_action.d4pg import D4PG
-from .agent.continuous_action.sac_DrQ import SACDrQ
 
 agents = {
     'ddpg': DDPG,
@@ -14,5 +12,4 @@ agents = {
     'sac_her': GoalConditionedSAC,
     'td3': TD3,
     'distri_ddpg': DistributionalDDPG,
-    'd4pg': D4PG
 }
