ad57a115c495570c3d51bfec66c3f56225bef3d0
xintong <xintongyang1994@foxmail.com>
2024-08-26 15:16:51 +0100

    implementing pointnet sac

---
+++ drl_implementation/agent/continuous_action/sac_pointnet.py
@@ -0,0 +1,204 @@
+import time
+import numpy as np
+import torch as T
+import torch.nn.functional as F
+from torch.optim.adam import Adam
+from ..utils.networks_mlp import StochasticActor, Critic
+from ..utils.networks_pointnet import CriticPointNet, CriticPointNet2
+from ..agent_base import Agent
+
+
+class SAC(Agent):
+    def __init__(self, algo_params, env, transition_tuple=None, path=None, seed=-1):
+        # environment
+        self.env = env
+        self.env.seed(seed)
+        obs = self.env.reset()
+        algo_params.update({'state_dim': obs.shape[0],
+                            'action_dim': self.env.action_space.shape[0],
+                            'action_max': self.env.action_space.high,
+                            'action_scaling': self.env.action_space.high[0],
+                            'init_input_means': None,
+                            'init_input_vars': None
+                            })
+        # training args
+        self.training_episodes = algo_params['training_episodes']
+        self.testing_gap = algo_params['testing_gap']
+        self.testing_episodes = algo_params['testing_episodes']
+        self.saving_gap = algo_params['saving_gap']
+
+        super(SAC, self).__init__(algo_params,
+                                  action_type='continuous',
+                                  transition_tuple=transition_tuple,
+                                  goal_conditioned=False,
+                                  path=path,
+                                  seed=seed)
+        # torch
+        self.network_dict.update({
+            'actor': StochasticActor(2048, self.action_dim,
+                                     fc1_size=1024, log_std_min=-6, log_std_max=1,
+                                     action_scaling=self.action_scaling).to(self.device),
+            'critic_1': CriticPointNet(output_dim=1, action_dim=self.action_dim).to(self.device),
+            'critic_1_target': CriticPointNet(output_dim=1, action_dim=self.action_dim).to(self.device),
+            'critic_2': CriticPointNet(output_dim=1, action_dim=self.action_dim).to(self.device),
+            'critic_2_target': CriticPointNet(output_dim=1, action_dim=self.action_dim).to(self.device),
+            'alpha': algo_params['alpha'],
+            'log_alpha': T.tensor(np.log(algo_params['alpha']), requires_grad=True, device=self.device),
+        })
+        self.network_keys_to_save = ['actor', 'critic_1_target']
+        self.actor_optimizer = Adam(self.network_dict['actor'].parameters(), lr=self.actor_learning_rate)
+        self.critic_1_optimizer = Adam(self.network_dict['critic_1'].parameters(), lr=self.critic_learning_rate)
+        self.critic_2_optimizer = Adam(self.network_dict['critic_2'].parameters(), lr=self.critic_learning_rate)
+        self._soft_update(self.network_dict['critic_1'], self.network_dict['critic_1_target'], tau=1)
+        self._soft_update(self.network_dict['critic_2'], self.network_dict['critic_2_target'], tau=1)
+        self.target_entropy = -self.action_dim
+        self.alpha_optimizer = Adam([self.network_dict['log_alpha']], lr=self.actor_learning_rate)
+        # training args
+        self.warmup_step = algo_params['warmup_step']
+        self.actor_update_interval = algo_params['actor_update_interval']
+        self.critic_target_update_interval = algo_params['critic_target_update_interval']
+        # statistic dict
+
+    def run(self, test=False, render=False, load_network_ep=None, sleep=0):
+        if test:
+            num_episode = self.testing_episodes
+            if load_network_ep is not None:
+                print("Loading network parameters...")
+                self._load_network(ep=load_network_ep)
+            print("Start testing...")
+        else:
+            num_episode = self.training_episodes
+            print("Start training...")
+
+        for ep in range(num_episode):
+            ep_return = self._interact(render, test, sleep=sleep)
+            self.logger.add_scalar(tag='Task/return', scalar_value=ep_return, global_step=ep)
+            print("Episode %i" % ep, "return %0.1f" % ep_return)
+
+            if (ep % self.testing_gap == 0) and (ep != 0) and (not test):
+                ep_test_return = []
+                for test_ep in range(self.testing_episodes):
+                    ep_test_return.append(self._interact(render, test=True))
+                self.logger.add_scalar(tag='Task/test_return', scalar_value=(sum(ep_test_return) / self.testing_episodes), global_step=ep)
+                print("Episode %i" % ep, "test return %0.1f" % (sum(ep_test_return) / self.testing_episodes))
+
+            if (ep % self.saving_gap == 0) and (ep != 0) and (not test):
+                self._save_network(ep=ep)
+
+        if not test:
+            print("Finished training")
+            print("Saving statistics...")
+        else:
+            print("Finished testing")
+
+    def _interact(self, render=False, test=False, sleep=0):
+        done = False
+        obs = self.env.reset()
+        ep_return = 0
+        # start a new episode
+        while not done:
+            if render:
+                self.env.render()
+            if self.env_step_count < self.warmup_step:
+                action = self.env.action_space.sample()
+            else:
+                action = self._select_action(obs, test=test)
+            new_obs, reward, done, info = self.env.step(action)
+            time.sleep(sleep)
+            ep_return += reward
+            if not test:
+                self._remember(obs, action, new_obs, reward, 1 - int(done))
+                if (self.env_step_count % self.update_interval == 0) and (self.env_step_count > self.warmup_step):
+                    self._learn()
+                self.env_step_count += 1
+            obs = new_obs
+        return ep_return
+
+    def _select_action(self, obs, test=False):
+        obs_points = T.as_tensor([obs['observation']], dtype=T.float).to(self.device)
+        goal_points = T.as_tensor([obs['desired_goal']], dtype=T.float).to(self.device)
+        obs_point_features = self.network_dict['critic_1_target'].get_features(obs_points.transpose(2, 1))
+        goal_point_features = self.network_dict['critic_1_target'].get_features(goal_points.transpose(2, 1))
+        inputs = T.cat((obs_point_features, goal_point_features), dim=1)
+        action = self.network_dict['actor'].get_action(inputs, mean_pi=test).detach().cpu().numpy()
+        return action[0]
+
+    def _learn(self, steps=None):
+        # todo: need update
+        if len(self.buffer) < self.batch_size:
+            return
+        if steps is None:
+            steps = self.optimizer_steps
+
+        for i in range(steps):
+            if self.prioritised:
+                batch, weights, inds = self.buffer.sample(self.batch_size)
+                weights = T.tensor(weights).view(self.batch_size, 1).to(self.device)
+            else:
+                batch = self.buffer.sample(self.batch_size)
+                weights = T.ones(size=(self.batch_size, 1)).to(self.device)
+                inds = None
+
+            actor_inputs = self.normalizer(batch.state)
+            actor_inputs = T.as_tensor(actor_inputs, dtype=T.float32, device=self.device)
+            actions = T.as_tensor(batch.action, dtype=T.float32, device=self.device)
+            critic_inputs = T.cat((actor_inputs, actions), dim=1)
+            actor_inputs_ = self.normalizer(batch.next_state)
+            actor_inputs_ = T.as_tensor(actor_inputs_, dtype=T.float32, device=self.device)
+            rewards = T.as_tensor(batch.reward, dtype=T.float32, device=self.device).unsqueeze(1)
+            done = T.as_tensor(batch.done, dtype=T.float32, device=self.device).unsqueeze(1)
+
+            if self.discard_time_limit:
+                done = done * 0 + 1
+
+            with T.no_grad():
+                actions_, log_probs_ = self.network_dict['actor'].get_action(actor_inputs_, probs=True)
+                critic_inputs_ = T.cat((actor_inputs_, actions_), dim=1)
+                value_1_ = self.network_dict['critic_1_target'](critic_inputs_)
+                value_2_ = self.network_dict['critic_2_target'](critic_inputs_)
+                value_ = T.min(value_1_, value_2_) - (self.network_dict['alpha'] * log_probs_)
+                value_target = rewards + done * self.gamma * value_
+
+            self.critic_1_optimizer.zero_grad()
+            value_estimate_1 = self.network_dict['critic_1'](critic_inputs)
+            critic_loss_1 = F.mse_loss(value_estimate_1, value_target.detach(), reduction='none')
+            (critic_loss_1 * weights).mean().backward()
+            self.critic_1_optimizer.step()
+
+            if self.prioritised:
+                assert inds is not None
+                self.buffer.update_priority(inds, np.abs(critic_loss_1.cpu().detach().numpy()))
+
+            self.critic_2_optimizer.zero_grad()
+            value_estimate_2 = self.network_dict['critic_2'](critic_inputs)
+            critic_loss_2 = F.mse_loss(value_estimate_2, value_target.detach(), reduction='none')
+            (critic_loss_2 * weights).mean().backward()
+            self.critic_2_optimizer.step()
+
+            self.statistic_dict['critic_loss'].append(critic_loss_1.detach().mean())
+
+            if self.optim_step_count % self.critic_target_update_interval == 0:
+                self._soft_update(self.network_dict['critic_1'], self.network_dict['critic_1_target'])
+                self._soft_update(self.network_dict['critic_2'], self.network_dict['critic_2_target'])
+
+            if self.optim_step_count % self.actor_update_interval == 0:
+                self.actor_optimizer.zero_grad()
+                new_actions, new_log_probs = self.network_dict['actor'].get_action(actor_inputs, probs=True)
+                critic_eval_inputs = T.cat((actor_inputs, new_actions), dim=1)
+                new_values = T.min(self.network_dict['critic_1'](critic_eval_inputs),
+                                   self.network_dict['critic_2'](critic_eval_inputs))
+                actor_loss = (self.network_dict['alpha'] * new_log_probs - new_values).mean()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+
+                self.alpha_optimizer.zero_grad()
+                alpha_loss = (self.network_dict['log_alpha'] * (-new_log_probs - self.target_entropy).detach()).mean()
+                alpha_loss.backward()
+                self.alpha_optimizer.step()
+                self.network_dict['alpha'] = self.network_dict['log_alpha'].exp()
+
+                self.statistic_dict['actor_loss'].append(actor_loss.detach().mean())
+                self.statistic_dict['alpha'].append(self.network_dict['alpha'].detach())
+                self.statistic_dict['policy_entropy'].append(-new_log_probs.detach().mean())
+
+            self.optim_step_count += 1
