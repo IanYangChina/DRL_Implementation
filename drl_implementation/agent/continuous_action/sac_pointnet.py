d07cdf52b1ab9d0713acfe8abb76732a7f8f14e1
xintong <xintongyang1994@foxmail.com>
2024-09-17 14:44:09 +0100

    sac pointnet

--- drl_implementation/agent/continuous_action/sac_pointnet.py
+++ drl_implementation/agent/continuous_action/sac_pointnet.py
@@ -10,7 +10,7 @@ from ..utils.exploration_strategy import GaussianNoise
 from collections import namedtuple
 
 
-class OneStepSAC(Agent):
+class PointnetSAC(Agent):
     def __init__(self, algo_params, env, logging=None, transition_tuple=None, path=None, seed=-1):
         # environment
         self.env = env
@@ -22,6 +22,7 @@ class OneStepSAC(Agent):
                             'action_max': self.env.action_space.high,
                             'action_scaling': self.env.action_space.high[0],
                             })
+        self.onestep = True if self.env.horizon == 1 else False
         # training args
         self.cur_ep = 0
         self.warmup_step = algo_params['warmup_step']
@@ -32,14 +33,14 @@ class OneStepSAC(Agent):
         if transition_tuple is None:
             transition_tuple = namedtuple('transition',
                                           ['state', 'desired_goal', 'action', 'achieved_goal', 'reward'])
-        super(OneStepSAC, self).__init__(algo_params, non_flat_obs=True,
-                                         action_type='continuous',
-                                         transition_tuple=transition_tuple,
-                                         goal_conditioned=True,
-                                         path=path,
-                                         seed=seed,
-                                         logging=logging,
-                                         create_logger=True)
+        super(PointnetSAC, self).__init__(algo_params, non_flat_obs=True,
+                                          action_type='continuous',
+                                          transition_tuple=transition_tuple,
+                                          goal_conditioned=True,
+                                          path=path,
+                                          seed=seed,
+                                          logging=logging,
+                                          create_logger=True)
         # torch
         self.network_dict.update({
             'actor': StochasticActor(2048, self.action_dim,
@@ -53,6 +54,12 @@ class OneStepSAC(Agent):
         })
         self.network_dict['critic_target'].eval()
         self._soft_update(self.network_dict['critic_1'], self.network_dict['critic_target'], tau=1)
+        if not self.onestep:
+            self.network_dict.update(
+                {'critic_target_2': CriticPointNet(output_dim=1, action_dim=self.action_dim).to(self.device)})
+            self.network_dict['critic_target_2'].eval()
+            self._soft_update(self.network_dict['critic_2'], self.network_dict['critic_target_2'], tau=1)
+
         self.network_keys_to_save = ['actor', 'critic_1']
         self.actor_optimizer = Adam(self.network_dict['actor'].parameters(), lr=self.actor_learning_rate)
         self.critic_1_optimizer = Adam(self.network_dict['critic_1'].parameters(), lr=self.critic_learning_rate)
@@ -128,23 +135,44 @@ class OneStepSAC(Agent):
             print("Finished testing")
 
     def _interact(self, render=False, test=False, sleep=0):
-        # An episode has only one step
         obs = self.env.reset()
         if render:
             self.env.render()
-        if self.use_demonstrations and (self.cur_ep < self.n_demonstrate_episodes):
-            action = self.gaussian_noise(self.demonstration_action)
-        else:
-            action = self._select_action(obs, test=test)
-        obs_, reward, _, info = self.env.step(action)
-        time.sleep(sleep)
+        if self.onestep:
+            # An episode has only one step
+            if self.use_demonstrations and (self.cur_ep < self.n_demonstrate_episodes):
+                action = self.gaussian_noise(self.demonstration_action)
+            else:
+                action = self._select_action(obs, test=test)
+            obs_, reward, _, info = self.env.step(action)
+            time.sleep(sleep)
 
-        if not test:
-            self._remember(obs['observation'], obs['desired_goal'], action, obs_['achieved_goal'], reward,
-                           new_episode=True)
-            if self.total_env_step_count % self.update_interval == 0:
-                self._learn()
-            self.total_env_step_count += 1
+            if not test:
+                self._remember(obs['observation'], obs['desired_goal'], action, obs_['achieved_goal'], reward,
+                               new_episode=True)
+                if self.total_env_step_count % self.update_interval == 0:
+                    self._learn()
+                self.total_env_step_count += 1
+        else:
+            n = 0
+            done = False
+            new_episode = True
+            while not done:
+                if self.use_demonstrations and (self.cur_ep < self.n_demonstrate_episodes):
+                    action = self.gaussian_noise(self.demonstration_action[n])
+                else:
+                    action = self._select_action(obs, test=test)
+                obs_, reward, done, info = self.env.step(action)
+                time.sleep(sleep)
+
+                if not test:
+                    self._remember(obs['observation'], obs['desired_goal'], action, obs_['achieved_goal'], reward,
+                                   new_episode=new_episode)
+                    if self.total_env_step_count % self.update_interval == 0:
+                        self._learn()
+                    self.total_env_step_count += 1
+
+                new_episode = False
 
         return info
 
@@ -181,10 +209,22 @@ class OneStepSAC(Agent):
             goal = T.as_tensor(batch.desired_goal, dtype=T.float32, device=self.device).transpose(2, 1)
             actions = T.as_tensor(batch.action, dtype=T.float32, device=self.device)
             rewards = T.as_tensor(batch.reward, dtype=T.float32, device=self.device).unsqueeze(1)
+            if self.onestep:
+                values_target = rewards
+            else:
+                with T.no_grad():
+                    obs_ = T.as_tensor(batch.next_state, dtype=T.float32, device=self.device).transpose(2, 1)
+                    obs_features_ = self.network_dict['critic_1'].get_features(obs_, detach=True)
+                    goal_features = self.network_dict['critic_1'].get_features(goal, detach=True)
+                    actor_inputs = T.cat((obs_features_, goal_features), dim=1)
+                    new_actions = self.network_dict['actor'].get_action(actor_inputs)
+                    values_1 = self.network_dict['critic_target'](obs_, new_actions, goal)
+                    values_2 = self.network_dict['critic_target_2'](obs_, new_actions, goal)
+                    values_target = rewards + self.gamma * T.min(values_1, values_2)
 
             self.critic_1_optimizer.zero_grad()
             value_estimate_1 = self.network_dict['critic_1'](obs, actions, goal)
-            critic_loss_1 = F.mse_loss(value_estimate_1, rewards, reduction='none')
+            critic_loss_1 = F.mse_loss(value_estimate_1, values_target, reduction='none')
             (critic_loss_1 * weights).mean().backward()
             self.critic_1_optimizer.step()
 
@@ -194,7 +234,7 @@ class OneStepSAC(Agent):
 
             self.critic_2_optimizer.zero_grad()
             value_estimate_2 = self.network_dict['critic_2'](obs, actions, goal)
-            critic_loss_2 = F.mse_loss(value_estimate_2, rewards, reduction='none')
+            critic_loss_2 = F.mse_loss(value_estimate_2, values_target, reduction='none')
             (critic_loss_2 * weights).mean().backward()
             self.critic_2_optimizer.step()
 
@@ -227,7 +267,11 @@ class OneStepSAC(Agent):
 
             self.optim_step_count += 1
 
-        self._soft_update(self.network_dict['critic_1'], self.network_dict['critic_target'], tau=1)
+        if self.onestep:
+            self._soft_update(self.network_dict['critic_1'], self.network_dict['critic_target'], tau=1)
+        else:
+            self._soft_update(self.network_dict['critic_1'], self.network_dict['critic_target'], tau=self.tau)
+            self._soft_update(self.network_dict['critic_2'], self.network_dict['critic_target_2'], tau=self.tau)
 
         self.logger.add_scalar(tag='Critic/critic_1_loss', scalar_value=avg_critic_1_loss / steps,
                                global_step=self.cur_ep)
